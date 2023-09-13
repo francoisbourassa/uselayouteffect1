dfa52dedb473b4f188c5b5d1fd2ace917f88effb
francois bourassa <you@example.com>
2023-09-13 19:39:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useLayoutEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const [color, setColor] = useState("black");
+  const divRef = useRef(null);
+
+  useLayoutEffect(() => {
+    if (divRef.current) {
+      const divWidth = divRef.current.offsetWidth;
+
+      // Mesurer la largeur après que le DOM a été rendu
+      setWidth(divWidth);
+
+      // Ajuster la couleur du texte en fonction de la largeur
+      if (divWidth < 200) {
+        setColor("red");
+      } else if (divWidth < 400) {
+        setColor("orange");
+      } else {
+        setColor("green");
+      }
+    }
+  }, []);  // Notez le tableau de dépendances vide pour n'exécuter cela qu'au montage
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div 
+        ref={divRef} 
+        style={{width: '60%', margin: 'auto', backgroundColor: 'lightgrey', color}}
+      >
+        La largeur de cette div est {width}px
+      </div>
     </div>
   );
 }
